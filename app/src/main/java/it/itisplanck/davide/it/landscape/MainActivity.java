da0e662cd57e8af3c3f152b65efdc6f8f2df1cd6
Davide Chiappa <davide.app99@gmail.com>
2018-03-23 16:51:10 +0100

    second

--- app/src/main/java/it/itisplanck/davide/it/landscape/MainActivity.java
+++ app/src/main/java/it/itisplanck/davide/it/landscape/MainActivity.java
@@ -6,18 +6,36 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private TextView t;
+    private int[] numeri;
+    private int operazione;
+    private String s;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         t=findViewById(R.id.textView);
+        numeri=new int[2];
+    }
+    public void function(View v){
+        if(v.getId()==R.id.button16){
+
+        }
+        else{
+            switch (v.getId()){
+                case R.id.button12:
+                    operazione=1;
+                    t.setText(t.getText().toString()+"+");
+                    s="";
+            }
+        }
 
     }
-    public void buttons(View v){
-        if(v.getId()!=R.id.button16)
-                t.setText(t.getText().toString()+((Button)findViewById(v.getId())).getText());
-
+    public void numbers(View v){
+        s+=((Button)findViewById(v.getId())).getText().toString();
+        t.setText(t.getText().toString()+((Button)findViewById(v.getId())).getText().toString());
     }
 }
